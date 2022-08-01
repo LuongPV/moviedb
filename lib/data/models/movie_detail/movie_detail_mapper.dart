5638eaac5e5a50700138b902991fdea048cb7f24
LVP <luong.x91@gmail.com>
2022-08-01 07:17:27 +0700

    Implement clean architecture

---
+++ lib/data/models/movie_detail/movie_detail_mapper.dart
@@ -0,0 +1,60 @@
+import '../../../domain/models/movie_detail.dart' as domain;
+import '../base_mapper.dart';
+import 'movie_detail.dart' as data;
+
+class CastByMovieMapper
+    extends BaseMapper<data.MovieDetail, domain.MovieDetail> {
+  @override
+  domain.MovieDetail convert(data.MovieDetail inputType) {
+    return domain.MovieDetail(
+      inputType.adult,
+      inputType.backdropPath,
+      _convertBelongsToCollection(inputType.belongsToCollection),
+      inputType.budget,
+      _convertGenres(inputType.genres),
+      inputType.homepage,
+      inputType.id,
+      inputType.imdbId,
+      inputType.originalLanguage,
+      inputType.originalTitle,
+      inputType.overview,
+      inputType.popularity,
+      inputType.posterPath,
+      inputType.productionCompanies,
+      inputType.productionCountries,
+      inputType.releaseDate,
+      inputType.revenue,
+      inputType.runtime,
+      inputType.spokenLanguages,
+      inputType.status,
+      inputType.tagline,
+      inputType.title,
+      inputType.video,
+      inputType.voteAverage,
+      inputType.voteCount,
+    );
+  }
+
+  domain.BelongsToCollection? _convertBelongsToCollection(
+      data.BelongsToCollection? inputType) {
+    if (inputType == null) {
+      return null;
+    }
+    return domain.BelongsToCollection(
+      inputType.id,
+      inputType.name,
+      inputType.posterPath,
+      inputType.backdropPath,
+    );
+  }
+
+  List<domain.Genres>? _convertGenres(List<data.Genres>? inputType) {
+    if (inputType == null) {
+      return null;
+    }
+    return inputType.map((e) => domain.Genres(
+      e.id,
+      e.name,
+    )).toList();
+  }
+}

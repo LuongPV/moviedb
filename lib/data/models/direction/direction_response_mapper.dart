136cddcd94e3bd6795f874b78b5f219b0f118ae5
LVP <luong.x91@gmail.com>
2022-08-09 20:30:43 +0700

    Add files

--- lib/data/models/direction/direction_response_mapper.dart
+++ lib/data/models/direction/direction_response_mapper.dart
@@ -1,24 +1,20 @@
 import '../../../domain/models/direction_response.dart' as domain;
 import '../data_to_domain_mapper.dart';
+import 'direction_response.dart' as data;
 
-class DirectionResponseMapper with DataToDomainMapper<data.DirectionResponse, domain.DirectionResponse> {
+class DirectionResponseMapper
+    with DataToDomainMapper<data.DirectionResponse, domain.DirectionResponse> {
   @override
   domain.DirectionResponse convertToDomain(data.DirectionResponse inputType) {
     return domain.DirectionResponse(
-      inputType.adult,
-      inputType.alsoKnownAs,
-      inputType.biography,
-      inputType.birthday,
-      inputType.deathday,
-      inputType.gender,
-      inputType.homepage,
-      inputType.id,
-      inputType.imdbId,
-      inputType.knownForDepartment,
-      inputType.name,
-      inputType.placeOfBirth,
-      inputType.popularity,
-      inputType.profilePath,
+      routes: inputType.routes
+          ?.map(
+            (e) => domain.Routes(
+              overviewPolyline:
+                  domain.OverviewPolyline(points: e.overviewPolyline?.points),
+            ),
+          )
+          .toList(),
     );
   }
 }
